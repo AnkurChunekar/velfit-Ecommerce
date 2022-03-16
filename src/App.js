8c067d0b28c2e77096daf1cdfc4b518b4f01eca4
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-03-16 14:10:45 +0530

    Feat: add login and signup page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import { Homepage } from "./pages/index.js";
 
-
 function App() {
   return (
     <div className="App">
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,3 @@
 export { default as Homepage } from "./homepage/Homepage";
+export { default as Login } from "./authentication/Login";
+export { default as Signup } from "./authentication/Signup";
