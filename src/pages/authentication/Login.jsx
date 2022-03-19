2df04effc8b865b5b0081c4994ffd8d9be628770
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-03-19 18:40:42 +0530

    Refactor: Login page components

--- src/pages/authentication/Login.jsx
+++ src/pages/authentication/Login.jsx
@@ -1,36 +1,40 @@
 import "./Authentication.css";
 import { Link } from "react-router-dom";
+import { TextInput, PasswordInput } from "./components";
+import { useState } from "react";
+import axios from "axios";
 
 export default function Login() {
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+
   return (
     <>
       <main className="main-container flex ai-start jc-center">
         <form className="authentication-container flex flex-column ai-left p-md2 m-xs">
           <h1 className="title m-s m-rl0 fs-3 fw-600">Log in</h1>
-          <div className="input-wrapper m-xxxs m-rl0">
-            <label htmlFor="email">Email</label>
-            <input
-              type="text"
-              id="email"
-              placeholder="johndoe@gmail.com"
-              className="input p-xxs m-xxs m-rl0 bd-rad-sm"
-            />
-            <label className="success-msg m-xxs m-rl0">
-              Success! Email entered is correct.
-            </label>
-          </div>
-          <div className="input-wrapper m-xxxs m-rl0">
-            <label htmlFor="password">Password</label>
-            <input
-              id="password"
-              className="input p-xxs m-xxs m-rl0 bd-rad-sm"
-              type="password"
-              placeholder="password"
-            />
-            <label className="error-msg m-xxs m-rl0">
-              Wrong Password. Try again.
-            </label>
-          </div>
+
+          <TextInput
+            labelText="Email"
+            id="email"
+            name="email"
+            placeholder="johndoe@gmail.com"
+            userData={userData}
+            setUserData={setUserData}
+            type="email"
+          />
+
+          <PasswordInput
+            labelText="Password"
+            id="password"
+            name="password"
+            placeholder="password"
+            userData={userData}
+            setUserData={setUserData}
+          />
+
           <button type="submit" className="btn btn-primary m-xxs m-rl0">
             LOGIN
           </button>
@@ -41,12 +45,9 @@ export default function Login() {
             </Link>
             .
           </p>
-          <a
-            href=""
-            className="primary-color-text center-align-text m-xxs m-rl0"
-          >
+          <button className="primary-color-text center-align-text m-xxs m-rl0 transparent-bg">
             Use Guest Credentials.
-          </a>
+          </button>
         </form>
       </main>
     </>
