0ec509bc6910fc61c0a3397c637c4c6abbe729f6
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-03-24 15:19:14 +0530

    Refactor: cart helper functions

--- src/helpers/cartHelpers.js
+++ src/helpers/cartHelpers.js
@@ -2,14 +2,4 @@ const cartPriceCalculator = (cart) => {
 return cart.reduce((acc, curr) =>  ({ price: acc.price + ( curr.price * curr.qty ) }), { price: 0 });
 }
 
-console.log("hello");
-
 export { cartPriceCalculator };
-
-/* 
-
-cart = [
-    {price, qty},
-    { price, qty }
-]
-*/
