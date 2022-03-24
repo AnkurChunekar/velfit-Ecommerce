670786920f99eee0ce3a3ca029676a8be29582ac
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-03-24 14:07:23 +0530

    Refactor: add/remove to wishlist services, and wishlist reducer

--- src/redux/wishlistReducer.js
+++ src/redux/wishlistReducer.js
@@ -4,10 +4,6 @@ const wishlistReducer = (state, action) => {
     switch (action.type) {
         case "UPDATE_WISHLIST":
             return { wishlist: action.payload.wishlist };
-        case "ADD_TO_WISHLIST":
-            return { wishlist: action.payload.wishlist };
-        case "REMOVE_FROM_WISHLIST":
-            return { wishlist: action.payload.wishlist };
         case "RESET":
             return initialWishlistState;
         default:
