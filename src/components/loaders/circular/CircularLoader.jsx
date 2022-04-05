57e1d09cc16138d5760783cc157bba20bd362cb5
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-04-05 19:12:29 +0530

    Feat: Add circular loader

---
+++ src/components/loaders/circular/CircularLoader.jsx
@@ -0,0 +1,16 @@
+import "./CircularLoader.css";
+
+export function CircularLoader() {
+  return (
+    <div className="lds-roller">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
