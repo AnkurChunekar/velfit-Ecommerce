1af7dd128af2c0622f8463d89d89cbe923f427b8
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-04-05 19:12:50 +0530

    Feat: Add Horizontal Loader

---
+++ src/components/loaders/horizontal/HorizontalLoader.jsx
@@ -0,0 +1,12 @@
+import "./HorizontalLoader.css";
+
+export function HorizontalLoader() {
+  return (
+    <div className="lds-ellipsis">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
