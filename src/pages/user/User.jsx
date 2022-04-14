b13dffc8269bfad97258bef19189c753121d065e
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-04-14 12:51:22 +0530

    Refactor: User page tabs

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
-import { ProfileTab } from "./components/ProfileTab";
-import { AddressesTab } from "./components/AddressesTab";
-import { SettingsTab } from "./components/SettingsTab";
-import { OrdersTab } from "./components/OrdersTab";
+import { v4 as uuid } from "uuid";
+import { ProfileTab, AddressesTab, SettingsTab, OrdersTab } from "./components";
 import "./User.css";
+
+const tabButtonData = [
+  { id: uuid(), tabName: "Profile" },
+  { id: uuid(), tabName: "Addresses" },
+  { id: uuid(), tabName: "Orders" },
+  { id: uuid(), tabName: "Settings" },
+];
 
 export default function User() {
   const [currentTab, setCurrentTab] = useState("profile");
@@ -26,38 +31,17 @@ export default function User() {
   return (
     <div className="user-page">
       <header className="tab-header w-100pc center-align-text">
-        <button
-          onClick={() => setCurrentTab("profile")}
-          className={`tab-name p-s btn-unset ${
-            currentTab === "profile" ? "active" : ""
-          }`}
-        >
-          Profile
-        </button>
-        <button
-          onClick={() => setCurrentTab("addresses")}
-          className={`tab-name p-s btn-unset ${
-            currentTab === "addresses" ? "active" : ""
-          }`}
-        >
-          Addresses
-        </button>
-        <button
-          onClick={() => setCurrentTab("orders")}
-          className={`tab-name p-s btn-unset ${
-            currentTab === "orders" ? "active" : ""
-          }`}
-        >
-          Orders
-        </button>
-        <button
-          onClick={() => setCurrentTab("settings")}
-          className={`tab-name p-s btn-unset ${
-            currentTab === "settings" ? "active" : ""
-          }`}
-        >
-          Settings
-        </button>
+        {tabButtonData.map((item) => (
+          <button
+            key={item.id}
+            onClick={() => setCurrentTab(item.tabName.toLowerCase())}
+            className={`tab-name p-s btn-unset ${
+              currentTab === item.tabName.toLowerCase() ? "active" : ""
+            }`}
+          >
+            {item.tabName}
+          </button>
+        ))}
       </header>
 
       {getCurrentTab()}
---
+++ src/pages/user/components/index.js
@@ -0,0 +1,4 @@
+export { AddressesTab } from "./AddressesTab";
+export { OrdersTab } from "./OrdersTab";
+export { ProfileTab } from "./ProfileTab";
+export { SettingsTab } from "./SettingsTab";
