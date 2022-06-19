5fcdca1c9989f2965242e21e5b510e31f911a402
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-06-19 22:00:33 +0530

    Chore: remove console.logs

--- src/services/cartServices/removeFromCart.service.js
+++ src/services/cartServices/removeFromCart.service.js
@@ -2,12 +2,9 @@ import axios from "axios";
 
 const removeFromCartService = async ({ token, product }) => {
   try {
-    const response = await axios.delete(`/api/user/cart/${product._id}`, {
+    return await axios.delete(`/api/user/cart/${product._id}`, {
       headers: { authorization: token },
     });
-
-    console.log(response);
-    return response;
   } catch (error) {
     if (axios.isAxiosError(error)) {
       if (error && error.response) {
